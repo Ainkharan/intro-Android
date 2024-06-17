b1a86cb85af9225b7607d287d626eedfe3d8ecc2
Ainkharan <ainkharansoftdev@gmail.com>
2024-06-17 12:08:13 +0100

    Initial Commit

---
+++ app/src/main/java/com/northcoders/caculatorapp/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.caculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
